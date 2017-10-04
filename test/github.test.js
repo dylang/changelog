5a95fd8b86da1e239949ebe98990c38dfdd592bc
Ger Hobbelt <ger@hobbelt.com>
2017-10-04 15:03:40 +0200

    chai github and npm tests (#43) don't fail with obscure error report anymore: `expect.an.object` is not the correct use: http://chaijs.com/api/bdd/ --> `expect.an('object')`

--- test/github.test.js
+++ test/github.test.js
@@ -12,7 +12,7 @@ describe('github', function () {
 
     it('should have something come back for changelog', function (done) {
         github.changelog('dylang/changelog', 'latest').then(function (results) {
-            expect(results).to.be.an.object;
+            expect(results).to.be.an('object');
         })
         .catch(function(err){throw err;})
         .done(done);
@@ -20,7 +20,7 @@ describe('github', function () {
 
     it('should have something come back for commit messages', function (done) {
         github.commitMessages({repo: 'dylang/changelog'}).then(function (results) {
-            expect(results).to.be.an.object;
+            expect(results).to.be.an('object');
         })
         .catch(function(err){throw err;})
         .done(done);

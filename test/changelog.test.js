d37eb4437fdd51fa19b0464eb67a9993655d8779
Ger Hobbelt <ger@hobbelt.com>
2019-08-06 19:39:28 +0200

    - fix: return failures (Errors ~ thrown as exceptions) through the same promise as any positive result. - fix: typos in test descriptions - add tests for the fix to prevent regression
    # Conflicts:
    #	test/changelog.test.js

--- test/changelog.test.js
+++ test/changelog.test.js
@@ -1,5 +1,6 @@
 "use strict";
 var expect = require('chai').expect;
+var assert = require('chai').assert;
 var proxyquire = require('proxyquire');
 
 var changelog = proxyquire('../lib/changelog', {
@@ -20,7 +21,7 @@ xdescribe('changelog', function() {
         .done(done);
     });
 
-    it('should be able to fomat the results as markdown', function (done) {
+    it('should be able to format the results as markdown', function (done) {
         changelog.generate('changelog', 'latest')
             .then(changelog.markdown)
             .then(function (results) {
@@ -30,7 +31,7 @@ xdescribe('changelog', function() {
         .done(done);
     });
 
-    it('should be able to fomat the results for the terminal', function (done) {
+    it('should be able to format the results for the terminal', function (done) {
         changelog.generate('changelog', 'latest')
             .then(changelog.terminal)
             .then(function (results) {
@@ -39,4 +40,22 @@ xdescribe('changelog', function() {
         .catch(function(err){throw err;})
         .done(done);
     });
+
+    it('should also send errors like "no project" through a promise', function (done) {
+        var count = 0;
+        changelog.generate(null, null, 'latest').then(function (results) {
+            assert.ok(false, "should never get here");
+        })
+        .catch(function(err){
+            count++;
+            assert.equal(count, 1);
+            assert(err.message.includes("No project specified."));
+        })
+        .then(function () {
+            count++;
+            assert.equal(count, 2);
+        })
+        .done(done);
+    });
+
 });
